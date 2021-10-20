8c86748514fbadb43db7b5b9610db3dbfb8d6e72
Adisudirta <yanadisudirta@gmail.com>
2021-10-20 21:52:34 +0800

    make header and banner responsive

--- src/scripts/header-util.js
+++ src/scripts/header-util.js
@@ -1,5 +1,14 @@
 const toggleBtn = document.getElementById("burger-btn");
-const nav = document.querySelector("nav");
+const nav = document.querySelector("nav#mobile");
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  if (this.scrollY > 10) {
+    header.classList.add("background");
+  } else {
+    header.classList.remove("background");
+  }
+});
 
 toggleBtn.addEventListener("click", function () {
   this.classList.toggle("rotate");
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,4 @@
 import "regenerator-runtime"; /* for async await transpile */
 import "../styles/main.css";
+// import "./styles/responsive.css";
 import "./header-util.js";
